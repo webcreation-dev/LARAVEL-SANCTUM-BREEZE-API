60c383029695daed10c8eba91c78e13edb3e9120
Julian-root <adjibiadechinan@gmail.com>
2023-10-27 18:09:23 +0100

    Fix Authentication API

--- routes/auth.php
+++ routes/auth.php
@@ -11,15 +11,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
                 ->name('register');
-
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
                 ->middleware('guest')
                 ->name('login');
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'stores'])
                 ->middleware('guest')
                 ->name('password.email');
-
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->middleware('guest')
                 ->name('password.store');
@@ -27,11 +25,13 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
                 ->middleware(['auth', 'signed', 'throttle:6,1'])
                 ->name('verification.verify');
-
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
                 ->middleware(['auth', 'throttle:6,1'])
                 ->name('verification.send');
-
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+// OTHERS
+Route::post('/forgot', [RegisteredUserController::class, 'forgotPassword']);
+Route::post('/reset', [RegisteredUserController::class, 'resetPassword']);
