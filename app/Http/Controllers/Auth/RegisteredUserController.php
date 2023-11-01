c4f173d0d2b533a9afe3e4844ce0d4d11b4f7e70
Julian-root <adjibiadechinan@gmail.com>
2023-11-01 11:01:06 +0100

    Add Endpoints CRUD

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,7 +31,7 @@ class RegisteredUserController extends Controller
     {
         try {
             $request->validate([
-                'profil_id' => ['required'],
+                // 'profil_id' => ['required'],
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'password' => ['required', 'confirmed', Rules\Password::defaults()],
