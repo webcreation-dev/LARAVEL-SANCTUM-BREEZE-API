f314c51998a3e22328f2aa702599dfdaa5b34a14
Julian-root <adjibiadechinan@gmail.com>
2023-11-02 23:28:37 +0100

    Add Bearer Token

--- routes/app.php
+++ routes/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\SellController;
 use Illuminate\Support\Facades\Route;
