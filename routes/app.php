59b7b230413aaf269b571cf66bb946d5aa643342
Julian-root <adjibiadechinan@gmail.com>
2023-11-02 23:28:08 +0100

    Add Bearer Token

--- routes/app.php
+++ routes/app.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::resource('patients', PatientController::class);
     Route::resource('sells', SellController::class);
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
