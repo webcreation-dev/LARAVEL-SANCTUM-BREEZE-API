ad56471f5356e0497a04bf83bac163e0a2470faf
Julian-root <adjibiadechinan@gmail.com>
2024-01-10 14:03:34 +0100

    update database

--- database/migrations/2023_10_31_092422_create_patients_table.php
+++ database/migrations/2023_10_31_092422_create_patients_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('first_name');
             $table->string('email')->nullable();
+            
             $table->string('treatment');
             $table->string('phone_number');
             $table->string('frame');
